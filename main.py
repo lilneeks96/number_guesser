21a38352a91aa9b69dccb496c21cf489c90ba6ac
lilneeks <nickodc96@gmail.com>
2022-10-22 19:55:01 -0700

    cleaned up code

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-# import random
-#
-# n = random.randint(1,10)
-# print("I am thinking of a number between 1 and 10")
-#
-# running = True
-# while running:
-#
-#     guess_str = input("Take a guess ")
-#     guess = int(guess_str)
-#
-#     if guess == n:
-#         print("Well done, that is right!")
-#         running = False
-#
-#     elif guess < n:
-#         print("Try a bigger number")
-#     else:
-#         print("Try a smaller number")
-
 import random
 
 n = random.randint(1,10)
@@ -28,14 +8,12 @@ while True:
     try:
         guess_str = input("Enter a number ")
         guess = int(guess_str)
-        if guess ==  n:
+        if guess == n:
             print("Well done!")
             running = False
         elif guess < n:
-                print("Try a bigger number")
+            print("Try a bigger number")
         else:
-                print("Try a smaller number")
-
+            print("Try a smaller number")
     except ValueError:
-        print('Please enter a number.')
-
+        print("Please enter a number.")
